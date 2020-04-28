6dc42505c2a5925b73a2baa9b75640620e61caa1
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2020-04-28 15:05:04 +0700

    finishing

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/todos',
+    path: '/',
     name: 'Todos',
     component: Todos
   },
